3211709a829da442609def6b1be585d915cbb9d3
Siu07 <siu07crd@googlemail.com>
2014-01-22 16:27:49 +0000

    Basic flags
    Added flags for program and PID equipment.

--- MenuSystem/MenuSystem.ino
+++ MenuSystem/MenuSystem.ino
@@ -1,14 +1,21 @@
-//21/01/13
-//Version 0.0.0
+//22/01/13
+//Version 0.0.1
 #include <MenuBackend.h>
+#include <LiquidCrystal.h>
+
+//Display
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
+
+int programme =0, menuFlag =0;
+
 
 MenuBackend menu = MenuBackend(menuUseEvent,menuChangeEvent);
   MenuItem selectPrg = MenuItem("Select Program");
-    MenuItem prg = MenuItem("Program #");
+    MenuItem prgMenu = MenuItem("Program #");
       MenuItem runPrg = MenuItem("Program Run");
       MenuItem editPrg = MenuItem("Edit Program");
         MenuItem prgName = MenuItem("Program Name");
-          MenuItem prgStep = MenuItem("Program Steps");
+          MenuItem prgMenuStep = MenuItem("Program Steps");
             MenuItem prgSet = MenuItem("Program Setpoints");
       MenuItem delPrg = MenuItem("Delete Program");
         MenuItem delPrgConf = MenuItem("Delete Confirmation");
@@ -26,13 +33,15 @@ void menuSetup()
 {
   menu.getRoot().add(selectPrg);
     selectPrg.addRight(prg);
-      prg.addRight(runPrg);
+      prgMenu.addRight(runPrg);
+      prgMenu.addRight(editPrg);
+      prgMenu.addRight(delPrg);
         runPrg.addBefore(delPrg);
         runPrg.addAfter(editPrg);
         editPrg.addAfter(delPrg);
-        editPrg.addRight(prgName);
-          prgName.addRight(prgStep);
-            prgStep.addRight(prgSet);
+          editPrg.addRight(prgName);
+            prgName.addRight(prgStep);
+             prgMenuStep.addRight(prgSet);
         delPrg.addAfter(runPrg);
         delPrg.addRight(delPrgConf);
    selectPrg.addBefore(stat);
@@ -67,92 +76,208 @@ void menuUseEvent(MenuUseEvent used)    //This is where you define a behaviour f
   Serial.print("Menu use ");
   Serial.println(used.item.getName());
   if (used.item == selectPrg){
-    Serial.print("Select a Program 1-5");
+    //Possibly do Program Select Directly from here
+    menu.moveRight(); 
   }
   else{
-  if (used.item == prg){
-    //do something
-  }
-  else{
-    if (used.item == runPrg){
-      //do something
+    if (used.item == prgMenu){
+      programme++;
+      if (programme>5) programme=1;    //Change to varible based on number of programs created
+      Serial.print("Select Program: ");
+      switch (programme) {
+      case 1:
+        { // Programme One - Snack Salami
+          lcd.print("Snack Salami");
+          Serial.print(programme);
+          Serial.println(" Snack Salami");
+          break;
+        }
+      case 2:
+        { // Programme Two - Saucison Sec
+          lcd.print("Saucisson Sec");
+          Serial.print(programme);
+          Serial.println(" Saucisson Sec");
+          break;
+        }
+      case 3:
+        { // Programme three - Bacon
+          lcd.print("Bacon");
+          Serial.print(programme);
+          Serial.println(" Bacon");
+          break;
+        }
+      case 4:
+        { // Programme Four - Large Meat
+          lcd.print("Large Meat");
+          Serial.print(programme);
+          Serial.println(" Large Meat");
+          break;
+        }
+      case 5:
+        { // Programme Five - Calibration
+          lcd.print("Calibration");
+          Serial.print(programme);
+          Serial.println(" Calibration");
+          break;
+        }
+      }
     }
     else{
-      if (used.item == editPrg){
-        //do something
+      if (used.item == runPrg){
+        //Start up PID.
+        //need code to keep PID paused until this is selected
       }
       else{
-        if (used.item == prgName){
-          //do something
+        if (used.item == editPrg){
+          menu.moveRight(); 
         }
         else{
-          if (used.item == prgStep){
-            //do something
+          if (used.item == prgName){
+            Serial.print("Plug in PC to edit names");//do something
           }
           else{
-            if (used.item == prgSet){
-              //do something
+            if (used.item ==prgMenuStep){
+              //Select the step to edit
             }
             else{
-              if (used.item == delPrg){
-                //do something
+              if (used.item == prgSet){
+                //Select the Temperature and humidity setpoints
               }
               else{
-                if (used.item == delPrgConf){
+                if (used.item == delPrg){
                   //do something
                 }
                 else{
-                  if (used.item == settings){
-                    //do something
+                  if (used.item == delPrgConf){
+                    //conformation screen to prevent accidental deletion
                   }
                   else{
-                    if (used.item == heat){
-                      //do something
+                    if (used.item == settings){
+                      menu.moveRight(); 
                     }
                     else{
-                    if (used.item == P){
-                      //do something
+                      if (used.item == heat){
+                        menuFlag = 1;
+                        menu.moveRight();
+                      }
+                      else{
+                        if (used.item == P){    //set Kp value for equipment
+                          if(menuFlag == 1) {
+                            Serial.println("Kp Heater");
+                          }
+                          else{
+                            if(menuFlag == 2) {
+                              Serial.println("Kp Cooler");
+                            }
+                            else{
+                              if(menuFlag == 3) {
+                                Serial.println("Kp Humidifier");
+                              }
+                              else{
+                                if(menuFlag == 4) {
+                                  Serial.println("Kp Dehumidifier");
+                                }
+                              }
+                            }
+                          }
+                          //SetTunings(newKp, Ki, Kd);
+                        }
+                        else{
+                          if (used.item == I){    //set Ki value for equipment
+                            if(menuFlag == 1) {
+                              Serial.println("Ki Heater");
+                            }
+                            else{
+                              if(menuFlag == 2) {
+                                Serial.println("Ki Cooler");
+                              }
+                              else{
+                                if(menuFlag == 3) {
+                                    Serial.println("Ki Humidifier");
+                                }
+                                else{
+                                  if(menuFlag == 4) {
+                                    Serial.println("Ki Dehumidifier");
+                                  }
+                                }
+                              }
+                            }
+                          }
+                          else{
+                            if (used.item == D){    //set Kd value for equipment
+                              if(menuFlag == 1) {
+                                Serial.println("Kd Heater");
+                              }
+                              else{
+                                if(menuFlag == 2) {
+                                  Serial.println("Kd Cooler");
+                                }
+                                else{
+                                  if(menuFlag == 3) {
+                                      Serial.println("Kd Humidifier");
+                                  }
+                                  else{
+                                    if(menuFlag == 4) {
+                                      Serial.println("Kd Dehumidifier");
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                            else{
+                              if (used.item == cool) {
+                                menuFlag = 2;
+                                menu.moveRight();
+                              }
+                              else{
+                                if (used.item == humid){
+                                  menuFlag = 3;
+                                  menu.moveRight();
+                                }
+                                else{
+                                  if (used.item == dehumid){
+                                    menuFlag = 4;
+                                    menu.moveRight();
+                                  }
+                                  else{
+                                    if (used.item == stat){
+                                      //Display current program, current temp & humidity, step and setpoints
+                                    }
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                      }
                     }
-                    else{
-                    if (used.item == I){
-                      //do something
-                    }
-                    else{
-                    if (used.item == D){
-                      //do something
-                    }
-                    else{
-                    if (used.item == cool){
-                      //do something
-                    }
-                    else{
-                    if (used.item == humid){
-                      //do something
-                    }
-                    else{
-                    if (used.item == dehumid){
-                      //do something
-                    }
-                    else{
-                    if (used.item == stat){
-                      //do something
-                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 void menuChangeEvent(MenuChangeEvent changed) //Notification of menu change
 {
-  Serial.print("Menu change ");
+  Serial.print("Menu change from ");
   Serial.print(changed.from.getName());
-  Serial.print(" ");
+  Serial.print(" to ");
   Serial.println(changed.to.getName());
 }
 
 void setup()
 {
   Serial.begin(9600);
-
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0);
   menuSetup();
   Serial.println("Starting navigation:\r\nUp: w   Down: s   Left: a   Right: d   Use: e");
+  menu.moveDown();
 }
 
 void loop()
