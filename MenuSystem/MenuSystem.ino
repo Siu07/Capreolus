da1db5cdc4ae0b8af4cf5f3af6ee190d765147a8
Siu07 <siu07crd@googlemail.com>
2014-01-24 17:42:26 +0000

    PID Memory
    Added memory to save PID settings between reboots.
    Added menu system to allow  changing PID settings through serial
    communication.

--- MenuSystem/MenuSystem.ino
+++ MenuSystem/MenuSystem.ino
@@ -1,17 +1,40 @@
-//22/01/13
-//Version 0.0.2
+//24/01/13
+//Version 0.0.3
 #include <MenuBackend.h>
 #include <LiquidCrystal.h>
+#include <PID_v1.h>
+#include <PID_AutoTune_v0.h>
+#include <EEPROMex.h>
 
 //Display
 LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 
-int programme =0, menuFlag =0;
+int programme =0, menuFlag = 0, numInput = 0;
+const int memBase          = 350;
 
+//heater                                      Define Default PID parameters            //need to set all to zero and read values from EEPROM
+double heInput, heOutput, heSetpoint;      //suggestions for next attempt      //on boot
+double heKp = 2, heKi = 1, heKd = 5;       //heKp = 2, heKi = 0.05, heKd = 5;
+int windowSize = 10000;
+unsigned long windowStartTime;
+//cooler 
+double coInput, coOutput, coSetpoint;
+double coKp = 0.2, coKi = 0.1, coKd = 2;  //coKp = 0.2, coKi = 0.01, coKd = 2;
+//humidifier
+double huInput, huOutput, huSetpoint;
+double huKp = 0.02, huKi = 0.01, huKd = 5;  //huKp = 0.02, huKi = 0.005, huKd = 5;
+//dehumidifier
+double deInput, deOutput, deSetpoint;
+double deKp = 0.02, deKi = 0.01, deKd = 5;  //deKp = 0.02, deKi = 0.01, deKd = 5;
+
+PID heater(&heInput, &heOutput, &heSetpoint, heKp, heKi, heKd, DIRECT);    //initilize PIDs
+PID cooler(&coInput, &coOutput, &coSetpoint, coKp, coKi, coKd, REVERSE);
+PID humidifier(&huInput, &huOutput, &huSetpoint, huKp, huKi, huKd, DIRECT);
+PID dehumidifier(&deInput, &deOutput, &deSetpoint, deKp, deKi, deKd, REVERSE);
 
 MenuBackend menu = MenuBackend(menuUseEvent,menuChangeEvent);
   MenuItem selectPrg = MenuItem("Select Program");
-    MenuItem prgMenu = MenuItem("Program #");
+    MenuItem prgMenu = MenuItem("Program");
       MenuItem runPrg = MenuItem("Program Run");
       MenuItem editPrg = MenuItem("Edit Program");
         MenuItem prgName = MenuItem("Program Name");
@@ -52,23 +75,193 @@ void menuSetup()
               P.addBefore(D);
               P.addAfter(I);
               I.addAfter(D);
+              I.addLeft(heat);
+              D.addLeft(heat);
             heat.addAfter(cool);
             cool.addRight(P);
               P.addBefore(D);
               P.addAfter(I);
               I.addAfter(D);
+              I.addLeft(cool);
+              D.addLeft(cool);
             cool.addAfter(humid);
             humid.addRight(P);
               P.addBefore(D);
               P.addAfter(I);
               I.addAfter(D);
+              I.addLeft(humid);
+              D.addLeft(humid);
             humid.addAfter(dehumid);
             dehumid.addRight(P);
               P.addBefore(D);
               P.addAfter(I);
               I.addAfter(D);
+              I.addLeft(dehumid);
+              D.addLeft(dehumid);
+            cool.addLeft(settings);
+            humid.addLeft(settings);
+            dehumid.addLeft(settings);
     settings.addAfter(stat);
     stat.addAfter(selectPrg);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0);
+  menuSetup();
+  Serial.println("Starting navigation:\r\nUp: w   Down: s   Left: a   Right: d   Use: e");
+  menu.moveDown();
+  
+  EEPROM.setMemPool(memBase, EEPROMSizeUno);
+  
+  heater.SetOutputLimits(0, windowSize);
+  heater.SetMode(AUTOMATIC); //Should be AUTOMATIC, only manual during testing
+  cooler.SetMode(AUTOMATIC);
+  humidifier.SetMode(AUTOMATIC);
+  dehumidifier.SetMode(AUTOMATIC);
+  
+  //Repopulate PID values from memory. Double occupies 4 bytes. Arduino Due board would require 8.
+  heKp = EEPROM.readDouble(0);
+  heKi = EEPROM.readDouble(4);
+  heKd = EEPROM.readDouble(8);
+  coKp = EEPROM.readDouble(12);
+  coKi = EEPROM.readDouble(16);
+  coKd = EEPROM.readDouble(20);
+  huKp = EEPROM.readDouble(24);
+  huKi = EEPROM.readDouble(28);
+  huKd = EEPROM.readDouble(32);
+  deKp = EEPROM.readDouble(36);
+  deKi = EEPROM.readDouble(40);
+  deKd = EEPROM.readDouble(44);
+}
+
+void loop()
+{
+  if (Serial.available()) {
+    byte read = Serial.read();
+    switch (read) {
+    case 'w': 
+      menu.moveUp(); 
+      break;
+    case 's': 
+      menu.moveDown(); 
+      break;
+    case 'd': 
+      menu.moveRight(); 
+      break;
+    case 'a': 
+      menu.moveLeft(); 
+      break;
+    case 'e': 
+      menu.use(); 
+      break;
+    }
+  }
+}
+
+void userInput() {
+  Serial.println("Enter a Value");
+  while(!Serial.available()) {    //Loop traps code until input is received
+    delay(100);
+  }
+   numInput = Serial.parseInt();
+   Serial.print("you entered: ");
+   Serial.println(numInput);
+  //Read the user's input
+  Serial.print("Previous value: ");
+  
+  switch (menuFlag) {  //case number, first digit: equipment type, second didgit: P,I or D
+    case 11: 
+      Serial.println(heKp);
+      heKp = EEPROM.updateDouble(0,numInput);
+      heater.SetTunings(numInput,heKi,heKd);// Heater Kp
+      menuFlag = menuFlag - 1;
+      Serial.print("Heater Kp changed to: ");
+      break;
+    case 12: 
+      Serial.println(heKi);
+      heKi = EEPROM.updateDouble(4,numInput);
+      heater.SetTunings(heKp,numInput,heKd);// Heater Ki
+      menuFlag = menuFlag - 2;
+      Serial.print("Heater Ki changed to: ");
+      break;
+    case 13: 
+      Serial.println(heKd);
+      heKd = EEPROM.updateDouble(8,numInput);
+      heater.SetTunings(heKp,heKi,numInput);// Heater Kd
+      menuFlag = menuFlag - 3;
+      Serial.print("Heater Kd changed to: ");
+      break;
+    case 21: 
+      Serial.println(coKp);
+      coKp = EEPROM.updateDouble(12,numInput);
+      cooler.SetTunings(numInput,coKi,coKd);// Cooler Kp
+      menuFlag = menuFlag - 1;
+      Serial.print("Cooler Kp changed to: ");
+      break;
+    case 22: 
+      Serial.println(coKi);
+      coKi = EEPROM.updateDouble(16,numInput);
+      cooler.SetTunings(coKp,numInput,coKd);// Cooler Ki
+      menuFlag = menuFlag - 2;
+      Serial.print("Cooler Ki changed to: ");
+      break;
+    case 23: 
+      Serial.println(coKd);
+      coKd = EEPROM.updateDouble(20,numInput);
+      cooler.SetTunings(coKp,coKi,numInput);// Cooler Kd
+      menuFlag = menuFlag - 3;
+      Serial.print("Cooler Kd changed to: ");
+      break;
+    case 31: 
+      Serial.println(huKp);
+      huKp = EEPROM.updateDouble(24,numInput);
+      humidifier.SetTunings(numInput,huKi,huKd);//Humidifier Kp
+      menuFlag = menuFlag - 1;
+      Serial.print("Humidifier Kp changed to: ");
+      break;
+    case 32: 
+      Serial.println(huKi);
+      huKi = EEPROM.updateDouble(28,numInput);
+      humidifier.SetTunings(huKp,numInput,huKd);// Humidifier Ki
+      menuFlag = menuFlag - 2;
+      Serial.print("Humidifier Ki changed to: ");
+      break;
+    case 33: 
+      Serial.println(huKd);
+      huKd = EEPROM.updateDouble(32,numInput);
+      humidifier.SetTunings(huKp,huKi,numInput);// Humidifier Kd
+      menuFlag = menuFlag - 3;
+      Serial.print("Humidifier Kd changed to: ");
+      break;
+    case 41: 
+      Serial.println(deKp);
+      deKp = EEPROM.updateDouble(36,numInput);
+      dehumidifier.SetTunings(numInput,deKi,deKd);// Dehumidifier Kp
+      menuFlag = menuFlag - 1;
+      Serial.print("Dehumidifier Kp changed to: ");
+      break;
+    case 42: 
+      Serial.println(deKi);
+      deKi = EEPROM.updateDouble(40,numInput);
+      dehumidifier.SetTunings(deKp,numInput,deKd);// Dehumidifier Ki
+      menuFlag = menuFlag - 2;
+      Serial.print("Dehumidifier Ki changed to: ");
+      break;
+    case 43: 
+      Serial.println(deKd);
+      deKd = EEPROM.updateDouble(44,numInput);//double update for double the fun.
+      dehumidifier.SetTunings(deKp,deKi,numInput);// Dehumidifier Kd
+      menuFlag = menuFlag - 3;
+      Serial.print("Dehumidifier Kd changed to: ");
+      break;
+     default:
+       Serial.println("Something went wrong!");
+      //
+  }
+  Serial.println(numInput);
 }
 
 void menuUseEvent(MenuUseEvent used)    //This is where you define a behaviour for a menu item
@@ -147,91 +340,52 @@ void menuUseEvent(MenuUseEvent used)    //This is where you define a behaviour f
                     }
                     else{
                       if (used.item == heat){
-                        menuFlag = 1;
+                        menuFlag = 10;
+                        Serial.println("Heater Selected");
                         menu.moveRight();
                       }
                       else{
-                        if (used.item == P){    //set Kp value for equipment
-                          if(menuFlag == 1) {
-                            Serial.println("Kp Heater");
-                          }
-                          else{
-                            if(menuFlag == 2) {
-                              Serial.println("Kp Cooler");
-                            }
-                            else{
-                              if(menuFlag == 3) {
-                                Serial.println("Kp Humidifier");
-                              }
-                              else{
-                                if(menuFlag == 4) {
-                                  Serial.println("Kp Dehumidifier");
-                                }
-                              }
-                            }
-                          }
-                          //SetTunings(newKp, Ki, Kd);
+                        if (used.item == cool) {
+                          menuFlag = 20;
+                          Serial.println("Cooler Selected");
+                          menu.moveRight();
                         }
                         else{
-                          if (used.item == I){    //set Ki value for equipment
-                            if(menuFlag == 1) {
-                              Serial.println("Ki Heater");
-                            }
-                            else{
-                              if(menuFlag == 2) {
-                                Serial.println("Ki Cooler");
-                              }
-                              else{
-                                if(menuFlag == 3) {
-                                    Serial.println("Ki Humidifier");
-                                }
-                                else{
-                                  if(menuFlag == 4) {
-                                    Serial.println("Ki Dehumidifier");
-                                  }
-                                }
-                              }
-                            }
+                          if (used.item == humid){
+                            menuFlag = 30;
+                            Serial.println("Humidifier Selected");
+                            menu.moveRight();
                           }
                           else{
-                            if (used.item == D){    //set Kd value for equipment
-                              if(menuFlag == 1) {
-                                Serial.println("Kd Heater");
-                              }
-                              else{
-                                if(menuFlag == 2) {
-                                  Serial.println("Kd Cooler");
-                                }
-                                else{
-                                  if(menuFlag == 3) {
-                                      Serial.println("Kd Humidifier");
-                                  }
-                                  else{
-                                    if(menuFlag == 4) {
-                                      Serial.println("Kd Dehumidifier");
-                                    }
-                                  }
-                                }
-                              }
+                            if (used.item == dehumid){
+                              menuFlag = 40;
+                              Serial.println("Dehumidifier Selected");
+                              menu.moveRight();
                             }
                             else{
-                              if (used.item == cool) {
-                                menuFlag = 2;
-                                menu.moveRight();
+                              if (used.item == stat){
+                                //Display current program, current temp & humidity, step and setpoints
                               }
                               else{
-                                if (used.item == humid){
-                                  menuFlag = 3;
-                                  menu.moveRight();
+                              if (used.item == P){    //set Kp value for equipment
+                                Serial.println("Kp Selected");
+                                menuFlag = menuFlag + 1;
+                                userInput();//input function
+                                //SetTunings(newKp, Ki, Kd);
                                 }
                                 else{
-                                  if (used.item == dehumid){
-                                    menuFlag = 4;
-                                    menu.moveRight();
+                                  if (used.item == I){    //set Ki value for equipment
+                                    Serial.println("Ki Selected");
+                                    menuFlag = menuFlag + 2;
+                                    userInput();//input function
+                                    //SetTunings(Kp, newKi, Kd);
                                   }
                                   else{
-                                    if (used.item == stat){
-                                      //Display current program, current temp & humidity, step and setpoints
+                                    if (used.item == D){    //set Kd value for equipment
+                                      Serial.println("Kd Selected");
+                                      menuFlag = menuFlag + 3;
+                                      userInput();//input function
+                                      //SetTunings(Kp, Ki, newKd);
                                     }
                                   }
                                 }
@@ -259,38 +413,3 @@ void menuChangeEvent(MenuChangeEvent changed) //Notification of menu change
   Serial.print(" to ");
   Serial.println(changed.to.getName());
 }
-
-void setup()
-{
-  Serial.begin(9600);
-  lcd.begin(20, 4);
-  lcd.setCursor(0, 0);
-  menuSetup();
-  Serial.println("Starting navigation:\r\nUp: w   Down: s   Left: a   Right: d   Use: e");
-  menu.moveDown();
-}
-
-void loop()
-{
-  if (Serial.available()) {
-    byte read = Serial.read();
-    switch (read) {
-    case 'w': 
-      menu.moveUp(); 
-      break;
-    case 's': 
-      menu.moveDown(); 
-      break;
-    case 'd': 
-      menu.moveRight(); 
-      break;
-    case 'a': 
-      menu.moveLeft(); 
-      break;
-    case 'e': 
-      menu.use(); 
-      break;
-    }
-  }
-}
-
